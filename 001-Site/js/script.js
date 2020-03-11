df626aaef4547216242da3f39c797566da1d4285
HgPBrito <hgbrito.tr@gmail.com>
2020-03-11 16:33:26 -0300

    implementação da validação de formulário

--- 001-Site/js/script.js
+++ 001-Site/js/script.js
@@ -2,20 +2,24 @@ function validateForm() {
 
     let nome = document.getElementById('nome-contato').value;
     let email = document.getElementById('email-contato').value;
-    let telefone = document.getElementById('tel-contato').value;
-    let mensagem = document.getElementById('msg-contato').value;
+    let tel = document.getElementById('tel-contato').value;
+    let msg = document.getElementById('msg-contato').value;
 
+    let valida = 0;
 
     if (nome.length < 1) {
-        document.getElementById("alert-name").innerHTML = "Preencha o Nome!";
+        document.getElementById("alert-name").innerHTML = "Preencha o nome!";
         document.getElementById("alert-name").style.visibility = "visible";
+        valida = 0;
     } else {
         document.getElementById("alert-name").style.visibility = "hidden";
+        valida++;
     }
 
     if (email.length < 1) {
-        document.getElementById("alert-email").innerHTML = "Preencha o Email!";
+        document.getElementById("alert-email").innerHTML = "Preencha o email!";
         document.getElementById("alert-email").style.visibility = "visible";
+        valida = 0;
 
     } else if (email.includes('@')) {
         let terminacoes = [".com", ".co", ".org", ".net", ".br"];
@@ -28,25 +32,35 @@ function validateForm() {
 
         if (tem) {
             document.getElementById("alert-email").style.visibility = "hidden";
+            valida++;
         } else {
-            document.getElementById("alert-email").innerHTML = "Invalid input field. Termination of invalid Email!";
+            document.getElementById("alert-email").innerHTML = "É uma terminação de email inválida!";
             document.getElementById("alert-email").style.visibility = "visible";
+            valida = 0;
         }
     } else {
-        document.getElementById("alert-email").innerHTML = "Invalid input field. Email Invalid!";
+        document.getElementById("alert-email").innerHTML = "É um email inválido!";
         document.getElementById("alert-email").style.visibility = "visible";
+        valida = 0;
     }
 
+    if (msg.length < 1) {
+        document.getElementById("alert-msg").innerHTML = "Mensagem inválida!";
+        document.getElementById("alert-msg").style.visibility = "visible";
+        valida = 0;
+    } else {
+        document.getElementById("alert-msg").style.visibility = "hidden";
+        valida++;
+    }
 
+    if (valida === 3) {
+        document.getElementById("alert-enviar").innerHTML = "Enviado com Sucesso!";
+        document.getElementById("alert-enviar").style.visibility = "visible";
+    } else {
+        document.getElementById("alert-enviar").style.visibility = "hidden";
+    }
 
 }
-
-
-
-
-
-
-
 
 document.getElementById('btn-enviar-contato').addEventListener('click', () => {
 
